d6b8a1294cfa448b74fb1efb56026579128f783b
Jacob Bieker <jacob@bieker.tech>
2019-04-18 18:22:00 +0200

    Add the examples for the vis

--- A2/1b_RNN/lstm_vis.py
+++ A2/1b_RNN/lstm_vis.py
@@ -6,14 +6,27 @@ import numpy
 Visualizations:
 
 Parameters Space of the Activations based on the inputs (characters? or sentences?)
+https://github.com/asap-report/lstm-visualisation/blob/master/lstm-visualisation.ipynb
+
 PCA then TNSE maybe?
+https://becominghuman.ai/visualizing-representations-bd9b62447e38
 
 Mappings of embeddings to outputs?
+https://medium.com/datalogue/attention-in-keras-1892773a4f22
+
 
 Compare Activations of mistranslated vs not mistranslated inputs
 
 Compare Activations of single language translator vs dual language translator
 
+Single neuron activations
+http://blog.echen.me/2017/05/30/exploring-lstms/
+
+Visualize internal states of the network?
+As in what is returned from return_state?
+
+How the network is workng:
+https://towardsdatascience.com/neural-machine-translation-using-seq2seq-with-keras-c23540453c74
 
 
 """
