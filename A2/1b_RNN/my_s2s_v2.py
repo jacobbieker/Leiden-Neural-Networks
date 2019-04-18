ed672dea04053ccb558b8720ed607185f460d7f0
Jacob Bieker <jacob@bieker.tech>
2019-04-18 16:53:48 +0200

    Add more German support
    The German model is being trained to not need any filtering. While worse than with filtering, it allows the code to be simpler, and reduces the need for dependencies such as nltk

--- A2/1b_RNN/my_s2s_v2.py
+++ A2/1b_RNN/my_s2s_v2.py
@@ -5,7 +5,6 @@ import numpy as np
 from gensim.models import KeyedVectors as w2v
 from re import match, sub
 
-
 class data_prep():
     def __init__(self, txtfile, num_samples=10000):
         self.data_path = txtfile
@@ -19,6 +18,7 @@ class data_prep():
         self.decoder_input_data = 'undef; run tokeniser function'
         self.decoder_target_data = 'undef; run tokeniser function'
         self.en_stop = ['a', 'and', 'of', 'to']
+
 
     def as_char(self):
         input_tokens = set()
@@ -86,10 +86,68 @@ class data_prep():
     def load_w2v(self):
         en_path = './w2v_models/GoogleNews-vectors-negative300.bin'
         fr_path = './w2v_models/fr_200_skip_cut100.bin'
+        de_path = './w2v_models/german.model'
         en_mod = w2v.load_word2vec_format(en_path, binary=True, limit=int(2E5))
         fr_mod = w2v.load_word2vec_format(fr_path, binary=True)
+        de_mod = w2v.load_word2vec_format(de_path, binary=True)
+        return en_mod, fr_mod, de_mod
 
-        return en_mod, fr_mod
+    def de_line_preproc(self, line, en_mod, de_mod):
+        skip_line = 0  # boolean if true, then skip line
+        input_text, target_text = line.split('\t')
+        # simplifications
+        if match('\$?[0-9]', line):  # skip numbers
+            skip_line = 1
+            return skip_line, _, _, _, _
+        if "'" in input_text:  # skip abbrev english sentences
+            skip_line = 1
+            return skip_line, _, _, _, _
+        # cannot is not in en dict, whisky is spelt whiskey
+        input_text = sub('cannot', 'can not', input_text)
+        input_text = sub('whisky', 'whiskey', input_text)
+        input_text = sub('[^\w ]', '', input_text)
+        target_text = sub("[^\w ]", '', target_text)
+        # de_mod requires no filtering
+        input_words = input_text.split()
+        target_words = target_text.split()
+
+        unk_line_inputs = []
+        unk_line_targets = []
+        input_vecs = np.empty((0, 300))
+        target_vecs = np.empty((0, 202))
+        for word in input_words:
+            word = sub('[^A-Za-z]', '', word)  # remove punctuation
+            if word == '':
+                continue
+            # 4 stop words have capitalised versions in dict
+            if word in self.en_stop:
+                word = word.capitalize()
+            if word not in en_mod.vocab:
+                word = word.lower()
+            if word not in en_mod.vocab:
+                skip_line = 2
+                unk_line_inputs.append((word, target_words))
+            else:
+                input_vecs = np.vstack((input_vecs, en_mod.word_vec(word)))
+        for word in target_words:
+            if word == '':
+                continue
+            if word not in de_mod.vocab:
+                skip_line = 2
+                unk_line_targets.append((word, target_words))
+            else:
+                vec = de_mod.word_vec(word)
+                vec = np.pad(vec, (2, 0), 'constant')
+                target_vecs = np.vstack((target_vecs, vec))
+        start_of_line = np.append([1, 0], np.zeros(200))
+        end_of_line = np.append([0, 1], np.zeros(200))
+        target_vecs = np.vstack((start_of_line, target_vecs, end_of_line))
+        if skip_line == 0:
+            self.input_texts.append(input_text)
+            self.target_texts.append("\t" + target_text + "\n")
+
+        return skip_line, input_vecs, target_vecs, unk_line_inputs, \
+               unk_line_targets
 
     def line_preproc(self, line, en_mod, fr_mod):
         skip_line = 0  # boolean if true, then skip line
@@ -155,7 +213,7 @@ class data_prep():
     def dryrun_wrdvec(self):  # show words not covered by the w2v models
         unk_input = []
         unk_target = []
-        en_mod, fr_mod = self.load_w2v()
+        en_mod, fr_mod, de_mod = self.load_w2v()
 
         with open(self.data_path, 'r', encoding='utf-8') as f:
             lines = f.read().split('\n')
@@ -170,7 +228,7 @@ class data_prep():
         return unk_input, unk_target
 
     def as_wrdvec(self):
-        en_mod, fr_mod = self.load_w2v()
+        en_mod, fr_mod, de_mod = self.load_w2v()
         in_max_len = 0
         tar_max_len = 0
         in_vec_list = []
