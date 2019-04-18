77417ef0c0954baf7bdf05ec0e12838ca48e0fc1
Jacob Bieker <jacob@bieker.tech>
2019-04-18 18:15:26 +0200

    Start on visualization stuff

---
+++ A2/1b_RNN/lstm_vis.py
@@ -0,0 +1,19 @@
+import keras
+import numpy
+
+"""
+
+Visualizations:
+
+Parameters Space of the Activations based on the inputs (characters? or sentences?)
+PCA then TNSE maybe?
+
+Mappings of embeddings to outputs?
+
+Compare Activations of mistranslated vs not mistranslated inputs
+
+Compare Activations of single language translator vs dual language translator
+
+
+
+"""
