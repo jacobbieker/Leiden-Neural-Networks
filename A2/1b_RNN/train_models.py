8824dec550457c9647f69c9ed770063ebb971d88
Yichao Li <yichaoli8@gmail.com>
2019-04-20 15:53:42 +0000

    fixed typos

--- A2/1b_RNN/train_models.py
+++ A2/1b_RNN/train_models.py
@@ -1,4 +1,4 @@
-import my_s2s_v2_noGenSim
+import my_s2s_v2_noGenSim as my
 '''
 list models: [{plain, rev}latent_dim, act_func, loss_func, latent_dim]
 '''
@@ -8,18 +8,18 @@ models=[["plain256", "softmax", "categorical_crossentropy", 256],
 
 # fr loop
 for model in models:
-    dat = data_prep("fra.txt", True)
+    dat = my.data_prep("fra.txt", True)
     dat.as_char()
-    mod = seq2seq(frdat)
+    mod = my.seq2seq(dat)
     mod.twolayer(model[1], model[2], model[3])
     mod.fit(model[0])
     a, i = mod.train_acc()
 
 # de loop
 for model in models:
-    dat = data_prep("deu.txt", True)
+    dat = my.data_prep("deu.txt", True)
     dat.as_char()
-    mod = seq2seq(frdat)
+    mod = my.seq2seq(dat)
     mod.twolayer(model[1], model[2], model[3])
     mod.fit(model[0])
     a, i = mod.train_acc()
