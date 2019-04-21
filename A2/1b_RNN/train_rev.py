f898221c3f8fd1cb8a290b125daa580a81d18833
Jacob Bieker <jacob@bieker.tech>
2019-04-21 10:19:27 +0200

    Update Training

--- A2/1b_RNN/train_rev.py
+++ A2/1b_RNN/train_rev.py
@@ -1,13 +1,17 @@
 import tensorflow as tf
-from keras.backend.tensorflow_backend import set_session
-config = tf.ConfigProto()
-config.gpu_options.per_process_gpu_memory_fraction = 0.3
-config.gpu_options.visible_device_list = "0"
-set_session(tf.Session(config=config))
 import my_s2s_v2 as my
+from keras import backend as K
 
-dat = my.data_prep("deu.txt")
-dat.as_wrdvec()
-mod_rev_vec = my.seq2seq(dat)
-mod_rev_vec.reverse("tanh", "mean_squared_error", 900)
-mod_rev_vec.fit("rev")
+act_functions = ["tanh", "linear"]
+losses = ["cosine", "mse", "mae", "logcosh"]
+paths = ["fra.txt", "deu.txt"]
+
+for act in act_functions:
+    for loss in losses:
+        for data in paths:
+            dat = my.data_prep(data, random_sample_flag=True, num_samples=10000)
+            dat.as_wrdvec()
+            mod_rev_vec = my.seq2seq(dat)
+            mod_rev_vec.reverse(act, loss, 900)
+            mod_rev_vec.fit("rev")
+            K.clear_session()
