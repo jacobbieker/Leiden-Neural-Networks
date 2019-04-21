3b68d64ace60738ee1b8338b5cec6d4ef8a380c2
Yichao Li <yichaoli8@gmail.com>
2019-04-21 07:48:05 +0000

    dslab file

--- A2/1b_RNN/train_models.py
+++ A2/1b_RNN/train_models.py
@@ -2,12 +2,6 @@ import my_s2s_v2_noGenSim as my
 '''
 list models: [{plain, rev}latent_dim, act_func, loss_func, latent_dim]
 '''
-dat = my.data_prep("fra.txt", True)
-dat.as_char()
-mod = my.seq2seq(dat)
-mod.twolayer("softmax", "categorical_crossentropy", 256)
-mod.loadwts("plain256_efr_char_softmax_categorical_crossentropy.h5")
-a, i = mod.train_acc()
 
 models=[["plain512", "softmax", "categorical_crossentropy", 512]]
 
