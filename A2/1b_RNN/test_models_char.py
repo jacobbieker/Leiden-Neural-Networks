9e53efba7f00783c7d25108b0674e05ef18ba871
Yichao Li <yichaoli8@gmail.com>
2019-04-21 19:05:20 +0000

    dslab

--- A2/1b_RNN/test_models_char.py
+++ A2/1b_RNN/test_models_char.py
@@ -6,21 +6,21 @@ models = ['plain256_efr_char_softmax_categorical_crossentropy.h5',
           'plain512_efr_char_softmax_categorical_crossentropy.h5']
 dim = [256, 512]
 
-for mod, d in zip(models, dim):
+for file, d in zip(models, dim):
     dat = my.data_prep("fra.txt", True)
     dat.as_char()
     mod = my.seq2seq(dat)
     mod.twolayer("softmax", "categorical_crossentropy", d)
-    mod.loadwts(mod)
+    mod.loadwts(file)
     a, i = mod.train_acc()
 
 models = ['plain256_ede_char_softmax_categorical_crossentropy.h5',
           'plain512_ede_char_softmax_categorical_crossentropy.h5']
 
-for mod, d in zip(models, dim):
+for file, d in zip(models, dim):
     dat = my.data_prep("deu.txt", True)
     dat.as_char()
     mod = my.seq2seq(dat)
     mod.twolayer("softmax", "categorical_crossentropy", d)
-    mod.loadwts(mod)
+    mod.loadwts(file)
     a, i = mod.train_acc()
